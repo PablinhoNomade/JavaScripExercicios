afd2aa4a8b443fb5abb6db4a9bef423d3ee779fd
pablinhonomade <pablinhonomade@gmail.com>
2022-10-05 22:51:43 -0300

    05/out/2022

--- 01- Escrever E Achar/CadeMeuNome.js
+++ 01- Escrever E Achar/CadeMeuNome.js
@@ -1,8 +1,1 @@
-const cade = ["Andre", "MeuNome", "Juliano", "Andrei"]
-
-function localDoNome(caixa) {
-    let index = caixa.indexOf("MeuNome")
-    console.log(caixa[index], index)
-
-}
-localDoNome(cade)
+const cade = ["Andre", "MeuNome", "Juliano", "Andrei"];
