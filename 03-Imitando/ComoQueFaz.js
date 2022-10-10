81d9d76996798c0d59d93b292a31c5bcf0631ac2
pablinhonomade <pablinhonomade@gmail.com>
2022-10-10 08:51:45 -0300

    10-out-2022

--- 03-Imitando/ComoQueFaz.js
+++ 03-Imitando/ComoQueFaz.js
@@ -3,11 +3,3 @@ const gato = "Miauu"
 const galo = "Cocorioco"
 const lobo = "Auuuuuuu!!!"
 
-function chamado(animal) {
-    console.log(animal)
-}
-
-chamado(cachorro)
-chamado(gato)
-chamado(galo)
-chamado(lobo)
